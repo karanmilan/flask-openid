c7993f8223b5eb6b9ea9c689908f1a90e922b80c
Karan Jain <karanmilan@gmail.com>
2014-11-26 16:23:32 +0530

    Add all subdirectories

---
+++ config.py
@@ -0,0 +1,19 @@
+WTF_CSRF_ENABLED = True
+SECRET_KEY = 'you-will-never-guess'
+
+WTF_CSRF_ENABLED = True
+SECRET_KEY = 'you-will-never-guess'
+
+OPENID_PROVIDERS = [
+    {'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id'},
+    {'name': 'Yahoo', 'url': 'https://me.yahoo.com'},
+    {'name': 'AOL', 'url': 'http://openid.aol.com/<username>'},
+    {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
+    {'name': 'Facebook','url': 'http://facebook.com'},
+    {'name': 'FAS', 'url': 'https://admin.fedoraproject.org/accounts/login?_csrf_token=c807cc99ca33f158e52fd8989c715d8b5e64c85e'}]
+
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
